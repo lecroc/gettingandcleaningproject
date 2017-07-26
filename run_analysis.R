8ef5e55b159e18172079cbf50fb4ebab18edc4cc
lecroc <clecrone@compasspoint-media.com>
2017-07-26 16:35:29 -0500

    Documented script and tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -3,12 +3,13 @@
 # Open libraries
 
 library(dplyr)
-library(tidyr)
-library(stringr)
 
-# Get Data
+# Section 1 Get Data
 
-file<-"Dataset.zip"
+file<-"Dataset.zip"  # store the zip file name to variable "file"
+
+## if the file does not exist in the working directory, download the file into the working directory.
+## if the file is already there, print "file already there" to the console
 
 if(!file.exists(file)){fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -18,60 +19,69 @@ unzip("Dataset.zip")} else("file already there")
 
 
 
-# read in features and activities
+# Section 2 Read in features and activities datasets
 
-features<-read.table("UCI HAR Dataset/features.txt")
+features<-read.table("UCI HAR Dataset/features.txt") # read in data features
 
-featurelabels<-as.character(features[,2])
+featurelabels<-as.character(features[,2])  # create a character vector of the feature names
 
-featurekeep<-grep(".*mean*|*.std*", featurelabels)
+featurekeep<-grep(".*mean*|*.std*", featurelabels, ignore.case = T) # create a position vector of the features I want to keep in the final data set ignore.case is important
 
-featurelabels<-featurelabels[featurekeep]
+featurelabels<-featurelabels[featurekeep] # subset the featurelabels to only those I want to keep
 
-featurelabels<-gsub('[-()]', '', featurelabels)
+featurelabels<-gsub('[-()]', '', featurelabels) # remove symbols from feature labels
 
-featurelabels<-tolower(featurelabels)
-
-
-
-activities<-read.table("UCI HAR Dataset/activity_labels.txt")
-
-activitylabels<-as.character(activities[,2])
-
-activitylabels<-tolower(activitylabels)
-
-activitylabels<-gsub("_","",activitylabels)
+featurelabels<-tolower(featurelabels) # remove uppercase letters from feature labels
 
 
-# Read in test and train data sets
 
-trainsub<-read.table("UCI HAR Dataset/train/subject_train.txt")
+activities<-read.table("UCI HAR Dataset/activity_labels.txt") # read in activity data set
 
-trainact<-read.table("UCI HAR Dataset/train/y_train.txt")
+activitylabels<-as.character(activities[,2]) # create a character vector of the activity names
 
-train<-read.table("UCI HAR Dataset/train/x_train.txt")[featurekeep]
+activitylabels<-tolower(activitylabels) # make the activity names all lower case
 
-train<-cbind(trainsub, trainact, train)
+activitylabels<-gsub("_","",activitylabels) # remove symbols from activity names
 
-testsub<-read.table("UCI HAR Dataset/test/subject_test.txt")
 
-testact<-read.table("UCI HAR Dataset/test/y_test.txt")
+# Section 3 Read in test and train data sets
 
-test<-read.table("UCI HAR Dataset/test/x_test.txt")[featurekeep]
+# Training data
 
-test<-cbind(testsub, testact, test)
+trainsub<-read.table("UCI HAR Dataset/train/subject_train.txt") # read in training set subjects
 
-alldata<-rbind(train, test)
+trainact<-read.table("UCI HAR Dataset/train/y_train.txt") # read in training set activities
 
-names(alldata)<-c("subject", "activity", featurelabels)
+train<-read.table("UCI HAR Dataset/train/x_train.txt")[featurekeep] # read in features, selecting only those I want to keep
 
-alldata$subject<-as.factor(alldata$subject)
+train<-cbind(trainsub, trainact, train) # combine all three data frames into one
 
-alldata$activity<-factor(alldata$activity, levels=activities[,1], labels=activitylabels)
+# Test data
+
+testsub<-read.table("UCI HAR Dataset/test/subject_test.txt") # read in test set subjects
+
+testact<-read.table("UCI HAR Dataset/test/y_test.txt") # read in test set activities
+
+test<-read.table("UCI HAR Dataset/test/x_test.txt")[featurekeep] # read in test set features, selecting only those I want to keep
+
+test<-cbind(testsub, testact, test) # combine all three data frames into one
+
+alldata<-rbind(train, test) # create a new data frame - alldata by combining the rows of test and train
+
+names(alldata)<-c("subject", "activity", featurelabels) # add tidy column names to alldata
+
+alldata$subject<-as.factor(alldata$subject) # convert the subject column to a factor
+
+alldata$activity<-factor(alldata$activity, levels=activities[,1], labels=activitylabels) # convert the activity column to a factor
+
+# Use chaining commands in dyplr package to summarize feature value means by subject and activity:
 
 ad1<-alldata %>%
      group_by(subject, activity) %>%
      summarise_all(funs(mean))
+
+
+# Write out ad1 data frame as tidydata.txt to working directory:
 
 write.table(ad1, file="./tidydata.txt", row.names=F)
 
