ec99a44e09e6508e5c05f6a34374644001c5f2d8
lecroc <clecrone@compasspoint-media.com>
2017-07-25 22:03:07 -0500

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,73 @@
+## Getting and Cleaning Data Course Project
+
+# Open libraries
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+# Get Data
+
+file<-"Dataset.zip"
+
+if(!file.exists(file)){fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+download.file(fileURL, destfile = "./Dataset.zip")
+
+unzip("Dataset.zip")} else(print("file already there"))
+
+
+
+# read in features and activities
+
+features<-read.table("UCI HAR Dataset/features.txt")
+
+featurelabels<-as.character(features[,2])
+
+featurekeep<-grep(".*mean*|*.std*", featurelabels)
+
+featurelabels<-featurelabels[featurekeep]
+
+featurelabels<-gsub('[-()]', '', featurelabels)
+
+featurelabels<-tolower(featurelabels)
+
+
+
+activities<-read.table("UCI HAR Dataset/activity_labels.txt")
+
+activitylabels<-as.character(activities[,2])
+
+activitylabels<-tolower(activitylabels)
+
+activitylabels<-gsub("_","",activitylabels)
+
+
+# Read in test and train data sets
+
+trainsub<-read.table("UCI HAR Dataset/train/subject_train.txt")
+
+trainact<-read.table("UCI HAR Dataset/train/y_train.txt")
+
+train<-read.table("UCI HAR Dataset/train/x_train.txt")[featurekeep]
+
+train<-cbind(trainsub, trainact, train)
+
+testsub<-read.table("UCI HAR Dataset/test/subject_test.txt")
+
+testact<-read.table("UCI HAR Dataset/test/y_test.txt")
+
+test<-read.table("UCI HAR Dataset/test/x_test.txt")[featurekeep]
+
+test<-cbind(testsub, testact, test)
+
+alldata<-rbind(train, test)
+
+names(alldata)<-c("subject", "activity", featurelabels)
+
+alldata$subject<-as.factor(alldata$subject)
+
+alldata$activity<-factor(alldata$activity, levels=activities[,1], labels=activitylabels)
+
+
+
